6856a3515d31c892bfef896a563ec79cd1a99b67
abhinavmaity <144161664+abhinavmaity@users.noreply.github.com>
2025-07-07 18:29:23 +0530

    Added Test Cases for Use Case 14

--- main_test.go
+++ main_test.go
@@ -4,48 +4,48 @@ import (
 	"testing"
 )
 
-func TestNotifyPoliceForBlueToyotaCars(t *testing.T) {
+func TestNotifyPoliceForBMWCars(t *testing.T) {
 	// Setup
 	lotA := NewParkingLot("A", 5)                   // Create a parking lot with 5 spots
 	lotB := NewParkingLot("B", 5)                   // Create another parking lot with 5 spots
 	handicapLot := NewParkingLot("Handicap Lot", 3) // Special lot for handicap cars
 	attendant := ParkingAttendant{Name: "Rahul"}
 
-	// Park a blue Toyota car in lot A
-	car1 := Car{LicensePlate: "BT123", Make: "Toyota", Model: "Camry", Color: "Blue", Size: "medium"}
+	// Park a black BMW car in lot A
+	car1 := Car{LicensePlate: "BMW123", Make: "BMW", Model: "X5", Color: "Black", Size: "large"}
 	attendant.DirectCarToLot([]*ParkingLot{lotA, lotB, handicapLot}, car1)
 
-	// Park a non-blue Toyota car in lot B
-	car2 := Car{LicensePlate: "GT456", Make: "Toyota", Model: "Corolla", Color: "Green", Size: "medium"}
+	// Park a non-BMW car in lot B
+	car2 := Car{LicensePlate: "GT456", Make: "Toyota", Model: "Camry", Color: "Green", Size: "medium"}
 	attendant.DirectCarToLot([]*ParkingLot{lotA, lotB, handicapLot}, car2)
 
-	// Park another blue Toyota car in handicap lot
-	car3 := Car{LicensePlate: "BT789", Make: "Toyota", Model: "Highlander", Color: "Blue", Size: "medium"}
+	// Park another black BMW car in handicap lot
+	car3 := Car{LicensePlate: "BMW789", Make: "BMW", Model: "3 Series", Color: "Black", Size: "medium"}
 	attendant.DirectCarToLot([]*ParkingLot{lotA, lotB, handicapLot}, car3)
 
-	// Notify police about the location of all blue Toyota cars
-	lotA.NotifyPolice("Toyota", "Blue")
-	lotB.NotifyPolice("Toyota", "Blue")
-	handicapLot.NotifyPolice("Toyota", "Blue")
+	// Notify police about the location of all BMW cars
+	lotA.NotifyPolice("BMW", "Black")
+	lotB.NotifyPolice("BMW", "Black")
+	handicapLot.NotifyPolice("BMW", "Black")
 }
 
-func TestNotifyPoliceWhenNoBlueToyotaCars(t *testing.T) {
+func TestNotifyPoliceWhenNoBMWCars(t *testing.T) {
 	// Setup
 	lotA := NewParkingLot("A", 5)                   // Create a parking lot with 5 spots
 	lotB := NewParkingLot("B", 5)                   // Create another parking lot with 5 spots
 	handicapLot := NewParkingLot("Handicap Lot", 3) // Special lot for handicap cars
 	attendant := ParkingAttendant{Name: "Rahul"}
 
-	// Park a non-blue car in lot A
-	car1 := Car{LicensePlate: "GT123", Make: "Toyota", Model: "Camry", Color: "Green", Size: "medium"}
+	// Park a non-BMW car in lot A
+	car1 := Car{LicensePlate: "GT123", Make: "Toyota", Model: "Camry", Color: "Blue", Size: "medium"}
 	attendant.DirectCarToLot([]*ParkingLot{lotA, lotB, handicapLot}, car1)
 
-	// Park a non-blue car in lot B
-	car2 := Car{LicensePlate: "RT456", Make: "Toyota", Model: "Corolla", Color: "Red", Size: "medium"}
+	// Park a non-BMW car in lot B
+	car2 := Car{LicensePlate: "RD456", Make: "Honda", Model: "Civic", Color: "Red", Size: "medium"}
 	attendant.DirectCarToLot([]*ParkingLot{lotA, lotB, handicapLot}, car2)
 
-	// Notify police about the location of all blue Toyota cars (expecting no cars)
-	lotA.NotifyPolice("Toyota", "Blue")
-	lotB.NotifyPolice("Toyota", "Blue")
-	handicapLot.NotifyPolice("Toyota", "Blue")
+	// Notify police about the location of all BMW cars (expecting no cars)
+	lotA.NotifyPolice("BMW", "Black")
+	lotB.NotifyPolice("BMW", "Black")
+	handicapLot.NotifyPolice("BMW", "Black")
 }
